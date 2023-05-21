3bbb9e2630460971f772dbc55f0aa77d4ac74d8d
ocaiohen <caiohencontato@gmail.com>
2023-05-21 20:44:15 -0300

    Equação sem raízes reais e "a" igual a zero

--- main.js
+++ main.js
@@ -10,19 +10,24 @@ const resolverEquacao = () =>{
     const valorB = parseFloat(inputB.value)
     const valorC = parseFloat(inputC.value)
     console.log(valorA,valorB,valorC)
-
-    const delta = valorB * valorB - 4 * valorA * valorC
-    if(delta < 0){
-        resultadoEquacao.innerHTML = "A equação não tem raízes reais!"
+    if(valorA != 0){
+        const delta = valorB * valorB - 4 * valorA * valorC
+        if(delta < 0){
+            resultadoX1.innerHTML = "A equação não tem raízes reais!"
+            resultadoX2.innerHTML = ""
+        }
+        else{
+            console.log(delta)
+            const x1 = (-1 * valorB + Math.sqrt(delta, 2)) / (2 * valorA)
+            const x2 = (-1 * valorB - Math.sqrt(delta, 2)) / (2 * valorA)
+            resultadoX1.innerHTML = `x1 = ${x1}`
+            resultadoX2.innerHTML = `x2 = ${x2}`
+        }   
     }
     else{
-        console.log(delta)
-        const x1 = (-1 * valorB + Math.sqrt(delta, 2)) / (2 * valorA)
-        const x2 = (-1 * valorB - Math.sqrt(delta, 2)) / (2 * valorA)
-        resultadoX1.innerHTML = `x1 = ${x1}`
-        resultadoX2.innerHTML = `x2 = ${x2}`
-    }   
-     
+        resultadoX1.innerHTML = "Se o coeficiente a é igual a 0, não é uma equação de segundo grau!"
+        resultadoX2.innerHTML = ""
+    }
 }
 
 botaoResolver.onclick = resolverEquacao
